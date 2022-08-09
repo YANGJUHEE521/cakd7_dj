78e34fd71ed6ac0c13f6d113e6931985ab3bdab3
YANGJUHEE521 <1986comatose@gmail.com>
2022-08-09 17:15:44 +0900

    help me

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostList(ListView):
     model = Post
     ordering = '-pk'  #최신 글부터 맨 위에 배치 
-    template_name= 'blog/index.html'
+    
 
 class PostDetail(DetailView):
     model = Post
