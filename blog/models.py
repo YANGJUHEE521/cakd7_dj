2ce65ad48c719fdd0c96b2c8bcb111b60e1a662b
YANGJUHEE521 <1986comatose@gmail.com>
2022-08-10 19:59:01 +0900

    category upload

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+        
     class Meta:
         verbose_name_plural = 'Categories'
+
+    
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -26,7 +32,7 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
 
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
