9c50572fad0253bbe8c6d7d54592b6089ce2c11f
YANGJUHEE521 <1986comatose@gmail.com>
2022-08-10 17:49:40 +0900

    admin upload

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,6 @@ from .models import Post, Category
 admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields: {'slug': ('name', )}
+    prepopulated_fields= {'slug': ('name', )}
 
 admin.site.register(Category, CategoryAdmin)
