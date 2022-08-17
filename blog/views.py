44762c7389ff05dec9522092c6e1dcf457db4381
YANGJUHEE521 <1986comatose@gmail.com>
2022-08-17 22:28:19 +0900

    댓글 수정, 삭제 기능 구현 완료

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from unicodedata import category
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Category, Post, Tag
+from django.shortcuts import get_object_or_404
+from .models import Category, Post, Tag, Comment
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 
@@ -97,7 +99,7 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
-    
+        context['comment_form'] = CommentForm
         return context
 
 def category_page(request, slug):
@@ -134,4 +136,40 @@ def tag_page(request, slug):
         }
     )
 
-# Create your views here.
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment 
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+
+        else:
+            raise PermissionDenied
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+        
